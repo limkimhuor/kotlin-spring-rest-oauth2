d2ba2002074444fb6e3515fed7dbab2b307a3730
Lim Kimhuor <kimhuorlim@gmail.com>
2017-12-15 05:56:37 +0700

    init commit

---
+++ src/main/kotlin/org/kotlin/demoOauth2/kotlinspringrestoauth2/KotlinSpringRestOauth2Application.kt
@@ -0,0 +1,11 @@
+package org.kotlin.demoOauth2.kotlinspringrestoauth2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringRestOauth2Application
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringRestOauth2Application>(*args)
+}
---
+++ src/test/kotlin/org/kotlin/demoOauth2/kotlinspringrestoauth2/KotlinSpringRestOauth2ApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.kotlin.demoOauth2.kotlinspringrestoauth2
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinSpringRestOauth2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
